ae97a716eb6f2a1c0b4619dc700a725a75a7390c
Andres-Mendez1034 <toonkirby01@gmail.com>
2026-06-03 22:26:03 -0500

    deploy

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/auth";
+const API_URL = "https://brandconnect.azurewebsites.net/api/auth";
 
 export const register = async (userData) => {
   const res = await axios.post(`${API_URL}/register`, userData);
